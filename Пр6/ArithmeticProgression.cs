ae7a007d5ae1683a9df6e6e20b55d6005d9d798d
ИСП-43 <-43@LAB24-01>
2021-12-03 10:30:33 +0300

    complite

--- Пр6/ArithmeticProgression.cs
+++ Пр6/ArithmeticProgression.cs
@@ -11,16 +11,18 @@ namespace Пр6
         double IIndexable.a => throw new NotImplementedException();
         double IIndexable.b => throw new NotImplementedException();
         double IIndexable.c => throw new NotImplementedException();
-        public ArithmeticProgression (double a, double c)
+        public ArithmeticProgression (double a, double c,double b)
         {
             _a = a;
             _c = c;
+            _b = b;
         }
         double _c;
         double _a;
+        double _b;
         public double GetCurrent()
         {
-            _a += _a;
+            _a = _a + _b;
             return _a;
         }
 
--- Пр6/Program.cs
+++ Пр6/Program.cs
@@ -11,19 +11,19 @@ namespace Пр6
         static void Main(string[] args)
         {
             double a, b, c;
+            Console.Write("Укажите первый элемент прогрессии: ");
             a =c= double.Parse(Console.ReadLine());
-            b= double.Parse(Console.ReadLine());
-            int i= int.Parse(Console.ReadLine());
-            ArithmeticProgression AP = new ArithmeticProgression(a,c);
+            Console.Write("Коэффициент прогрессии: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Количество элементов: ");
+            int i= int.Parse(Console.ReadLine())-1;
+            ArithmeticProgression AP = new ArithmeticProgression(a,c,b);
             GeometricProgression GP = new GeometricProgression(a, c, b);
-            Console.WriteLine(a);
+            Console.WriteLine("Геометрическая прогрессия\n{0}",a);
             PrintSeries(GP, i);
-            PrintIndexable(AP, i);
-            /*ISeries[] series = new ISeries[2];
-            series[0] = GP;
-            series[1] = AP;*/
-
-
+            Console.WriteLine("Арифметическая прогрессия\n{0}", a);
+            PrintSeries(AP, i);
+            Console.ReadKey();
         }
         public static void PrintSeries(ISeries series, int count)
         {
@@ -39,7 +39,6 @@ namespace Пр6
             {
                 indexable.ToString();
                 Console.WriteLine("{0}\n",indexable.GetType());
-
             }
         }
     }
